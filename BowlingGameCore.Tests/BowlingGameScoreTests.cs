883243123dfc680ceda33f81367b929890337897
Aymen BEN MILED <benmiledaymen@hotmail.com>
2022-11-10 13:08:10 +0100

    qdd spqre test

--- BowlingGameCore.Tests/BowlingGameScoreTests.cs
+++ BowlingGameCore.Tests/BowlingGameScoreTests.cs
@@ -31,5 +31,35 @@ namespace BowlingGameCore.Tests
             // Assert 
             Assert.Equal(300, curerntScrore);
         }
+        [Fact]
+        public void RollsNostrikeReturns_50()
+        {
+            // Arrange
+            Game game = new Game();
+            game.Rolls(new int[] { 0,5,5,0,0,5,5,0,0,5,5,0,0,5,5,0,0,5,5,0});
+
+
+            // Act
+            var curerntScrore = game.Score();
+
+
+            // Assert 
+            Assert.Equal(50, curerntScrore);
+        }
+        [Fact]
+        public void RollsSparesReturns_150()
+        {
+            // Arrange
+            Game game = new Game();
+            game.Rolls(new int[] { 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5});
+
+
+            // Act
+            var curerntScrore = game.Score();
+
+
+            // Assert 
+            Assert.Equal(150, curerntScrore);
+        }
     }
 }
