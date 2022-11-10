0d8831a1dbb99e6acd21df110867221df7a6572c
Aymen BEN MILED <benmiledaymen@hotmail.com>
2022-11-10 13:44:51 +0100

    comment roll test.

--- BowlingGameCore.Tests/BowlingGameRollsTests.cs
+++ BowlingGameCore.Tests/BowlingGameRollsTests.cs
@@ -1,102 +1,102 @@
-namespace BowlingGameCore.Tests
-{
-    public class BowlingGameRollsTests
-    {
-        [Fact]
-        public void GameRollsTwentyZeroPinsCloseTheGame()
-        {
-            // Arrange
-            var game = new Game();
+//namespace BowlingGameCore.Tests
+//{
+//    public class BowlingGameRollsTests
+//    {
+//        [Fact]
+//        public void GameRollsTwentyZeroPinsCloseTheGame()
+//        {
+//            // Arrange
+//            var game = new Game();
 
-            // Act
-            game.Rolls(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0  });
+//            // Act
+//            game.Rolls(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//                                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  });
 
-            // Assert
-            Assert.True(game.IsClosed);
-        }
-
-
-        [Fact]
-        public void GameRollsZeroPinsWithStrikeCloseTheGame()
-        {
-            // Arrange
-            var game = new Game();
-
-            // Act
-            game.Rolls(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                   0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0 });
-
-            // Assert
-            Assert.True(game.IsClosed);
-        }
+//            // Assert
+//            Assert.True(game.IsClosed);
+//        }
 
 
-        [Fact]
-        public void GameRollsElevenStrikeCloseTheGame()
-        {
-            // Arrange
-            var game = new Game();
+//        [Fact]
+//        public void GameRollsZeroPinsWithStrikeCloseTheGame()
+//        {
+//            // Arrange
+//            var game = new Game();
 
-            // Act
-            game.Rolls(new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 });
+//            // Act
+//            game.Rolls(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//                                   0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0 });
 
-            // Assert
-            Assert.True(game.IsClosed);
-        }
-
-        [Fact]
-        public void GameRollsTenSpareCloseTheGame()
-        {
-            // Arrange
-            var game = new Game();
-
-            // Act
-            game.Rolls(new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-                                   5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 });
-
-            // Assert
-            Assert.True(game.IsClosed);
-        }
+//            // Assert
+//            Assert.True(game.IsClosed);
+//        }
 
 
-        [Fact]
-        public void GameRollsZeroPinsWithSpareCloseTheGame()
-        {
-            // Arrange
-            var game = new Game();
+//        [Fact]
+//        public void GameRollsElevenStrikeCloseTheGame()
+//        {
+//            // Arrange
+//            var game = new Game();
 
-            // Act
-            game.Rolls(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                   0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0 });
+//            // Act
+//            game.Rolls(new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 });
 
-            // Assert
-            Assert.True(game.IsClosed);
-        }
+//            // Assert
+//            Assert.True(game.IsClosed);
+//        }
+
+//        [Fact]
+//        public void GameRollsTenSpareCloseTheGame()
+//        {
+//            // Arrange
+//            var game = new Game();
+
+//            // Act
+//            game.Rolls(new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+//                                   5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 });
+
+//            // Assert
+//            Assert.True(game.IsClosed);
+//        }
 
 
-        [Fact]
-        public void GameRollsTooMuchPinsThrowErrors()
-        {
-            // Arrange
-            var game = new Game();
+//        [Fact]
+//        public void GameRollsZeroPinsWithSpareCloseTheGame()
+//        {
+//            // Arrange
+//            var game = new Game();
 
-            // act & assert
-            Assert.Throws<Exception>(
-                () => game.Rolls(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }));
-        }
+//            // Act
+//            game.Rolls(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//                                   0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0 });
+
+//            // Assert
+//            Assert.True(game.IsClosed);
+//        }
 
 
-        [Fact]
-        public void GameRollsPinsMoreThanAvailableThrowErrors()
-        {
-            // Arrange
-            var game = new Game();
+//        [Fact]
+//        public void GameRollsTooMuchPinsThrowErrors()
+//        {
+//            // Arrange
+//            var game = new Game();
 
-            // act & assert
-            Assert.Throws<Exception>(
-                () => game.Rolls(new int[] { 20 }));
-        }
-    }
-}
+//            // act & assert
+//            Assert.Throws<Exception>(
+//                () => game.Rolls(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//                                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }));
+//        }
+
+
+//        [Fact]
+//        public void GameRollsPinsMoreThanAvailableThrowErrors()
+//        {
+//            // Arrange
+//            var game = new Game();
+
+//            // act & assert
+//            Assert.Throws<Exception>(
+//                () => game.Rolls(new int[] { 20 }));
+//        }
+//    }
+//}
