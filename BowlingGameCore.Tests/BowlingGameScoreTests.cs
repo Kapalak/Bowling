7277046d054eefc621c3c288c970ff86af68d3db
Aymen BEN MILED <benmiledaymen@hotmail.com>
2022-11-10 12:32:47 +0100

    Create first test

--- BowlingGameCore.Tests/BowlingGameScoreTests.cs
+++ BowlingGameCore.Tests/BowlingGameScoreTests.cs
@@ -3,13 +3,18 @@ namespace BowlingGameCore.Tests
     public class BowlingGameScoreests
     {
         [Fact]
-        public void WhatIsTheBestTestName()
+        public void RollsAllZeroReturnsZero()
         {
             // Arrange
+            Game game = new Game();
+            game.Rolls(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
 
             // Act
+            var curerntScrore = game.Score();
 
-            // Assert
+
+            // Assert 
+            Assert.Equal(0, curerntScrore);
         }
     }
 }
