d4d48b8ec3e6a79742f56f867d8c596c569dbe07
Aymen BEN MILED <benmiledaymen@hotmail.com>
2022-11-10 11:47:14 +0100

    Add empty test class.

---
+++ BowlingGameCore.Tests/BowlingGameScoreTests.cs
@@ -0,0 +1,15 @@
+namespace BowlingGameCore.Tests
+{
+    public class BowlingGameScoreests
+    {
+        [Fact]
+        public void WhatIsTheBestTestName()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
